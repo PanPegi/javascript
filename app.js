407d09d9ea38111c59139db2420b8fb900e34bb1
PanPegi <126827850+PanPegi@users.noreply.github.com>
2023-03-09 13:57:22 +0100

    1 file modified

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from 'express';
 
 const app = express();
+app.get('/', (req, res) => {
+  const neco = { name: 'vojta' };
+  res.json(neco);
+});
+
 app.listen(3000, () => {
   console.log('cau');
 });
